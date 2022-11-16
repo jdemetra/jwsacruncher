1bf5b5526b316fce9d1115833b3d79799d9e0269
Philippe Charles <philippe.charles@nbb.be>
2022-11-16 08:12:15 +0100

    Remove junit-vintage-engine dependency

--- src/test/java/ec/jwsacruncher/AppTest.java
+++ src/test/java/ec/jwsacruncher/AppTest.java
@@ -1,60 +1,44 @@
 /*
-* Copyright 2013 National Bank of Belgium
-*
-* Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
-* by the European Commission - subsequent versions of the EUPL (the "Licence");
-* You may not use this work except in compliance with the Licence.
-* You may obtain a copy of the Licence at:
-*
-* http://ec.europa.eu/idabc/eupl
-*
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the Licence is distributed on an "AS IS" basis,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the Licence for the specific language governing permissions and 
-* limitations under the Licence.
+ * Copyright 2013 National Bank of Belgium
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
  */
 package ec.jwsacruncher;
 
-import java.io.File;
+import nbbrd.io.xml.bind.Jaxb;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import java.io.IOException;
-import java.io.StringWriter;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-    @Rule
-    public TemporaryFolder temp = new TemporaryFolder();
-
     @Test
-    public void testGenerateDefaultConfigFile() throws IOException {
-        File userDir = temp.newFolder("NoArgs");
+    public void testGenerateDefaultConfigFile(@TempDir Path temp) throws IOException {
+        App.generateDefaultConfigFile(temp.toFile());
 
-        App.generateDefaultConfigFile(userDir);
-
-        assertThat(new File(userDir, WsaConfig.DEFAULT_FILE_NAME))
+        assertThat(temp.resolve(WsaConfig.DEFAULT_FILE_NAME))
                 .hasContent(writeConfigToString(WsaConfig.generateDefault()));
     }
 
     private static String writeConfigToString(WsaConfig config) throws IOException {
-        try {
-            JAXBContext context = JAXBContext.newInstance(WsaConfig.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            StringWriter writer = new StringWriter();
-            marshaller.marshal(config, writer);
-            return writer.toString();
-        } catch (JAXBException ex) {
-            throw new IOException(ex);
-        }
+        return Jaxb.Formatter.of(WsaConfig.class).withFormatted(true).formatToString(config);
     }
 }
